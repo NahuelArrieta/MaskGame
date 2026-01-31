e5d3157f39173716198ea391845e53164e386489
NahuelArrieta <nahuel.arrietamza@gmail.com>
2026-01-31 16:07:22 -0300

    add tile map to level

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -11,7 +11,7 @@ func _physics_process(delta):
 
 	if is_on_floor() and Input.is_action_just_pressed("JUMP"):
 		velocity.y = jump_velocity
-		$Jump.play()
+		# $Jump.play()
 	
 
 	var direction = Input.get_axis("LEFT",  "RIGHT") 
